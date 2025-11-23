2c97b03d780624fac4b6fca97f9506d87fd1ba00
Dama5323 <your-deenyashke@gmail.com>
2025-11-23 18:57:56 +0300

    feat: implement Google Fonts integration

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -1,7 +1,9 @@
 import Button from "@/components/common/Button";
 import { useRouter } from "next/router";
-import { PageRouteProps } from "@/interface";
 
+interface PageRouteProps {
+  pageRoute: string
+}
 
 export default function Home() {
   const router = useRouter()
